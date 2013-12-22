874bf9b99a8365a94f4c1550e65af2b90222f4f8
Gordon Zhu <gordonmzhu@gmail.com>
2013-12-22 14:43:08 -0800

    Basic add project workflow to angular app

--- client/angular-seed/app/js/controllers.js
+++ client/angular-seed/app/js/controllers.js
@@ -3,9 +3,27 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
 
+  controller('MyCtrl1', ['$scope', function($scope) {
+  	$scope.testVariable = 'hello there';
+
+  	$scope.projects = [
+  		{title: 'Bike lane', description: 'Widen bike lane'}
+  	];
+
+  	$scope.addProject = function() {
+  		// $http.post('/project', project).success(
+  		if (!$scope.title || !$scope.description) {
+  			return;
+  		}
+		$scope.projects.push({title: $scope.title, description: $scope.description});
+  		// );
+  		$scope.title = '';
+  		$scope.description = '';
+  	};
   }])
-  .controller('MyCtrl2', [function() {
+
+  .controller('MyCtrl2', ['$scope', function($scope) {
+	$scope.testVariable2 = 'hello there'; 
 
   }]);
