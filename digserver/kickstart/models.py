832d47f40090e6f2fdd3b33fecc03741daff3800
Jason Mylen <jmylen@Jasons-MacBook-Air.local>
2013-12-22 14:39:24 -0800

    adding amount_raised

--- digserver/kickstart/models.py
+++ digserver/kickstart/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Project(models.Model):
   title = models.CharField(max_length=140)
   description = models.TextField()
+  amount_raised = models.FloatField()
--- digserver/kickstart/serializers.py
+++ digserver/kickstart/serializers.py
@@ -4,5 +4,5 @@ from rest_framework import serializers
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = models.Project
-    fields = ('title', 'description') 
+    fields = ('title', 'description', 'amount_raised') 
 
